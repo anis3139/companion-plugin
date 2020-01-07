d140df405f1bd05b3f1229c6fa9f2c89efeb32d5
anis Arronno <anis904692@gmail.com>
2020-01-07 23:08:49 +0600

    Add Dowbole Section

--- companion.php
+++ companion.php
@@ -86,7 +86,42 @@ function mealc_register_my_cpts_section() {
 		"menu_icon" => "dashicons-media-document",
 		"supports" => [ "title"],
 	];
+	register_post_type( "reservation", $args );
+    
+	$labels = [
+		"name" => __( "recipes", "meal" ),
+		"singular_name" => __( "recipe", "meal" ),
+		"featured_image"=>__('Recipe Photo', 'meal')
+	];
 
+	$args = [
+		"label" => __( "recipes", "meal" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "recipe", "with_front" => true ],
+		"query_var" => true,
+		"menu_position" => 5,
+		"menu_icon" => "dashicons-smiley",
+		"supports" => [ "title", "editor", "thumbnail","excerpt"],
+		"taxonomies" => array('category'),
+	];
+
+	register_post_type( "recipe", $args );
 }
 
 add_action( 'init', 'mealc_register_my_cpts_section' );
